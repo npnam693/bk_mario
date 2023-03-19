99d2488fbf6bc31338b9b40beb9af26d68506fd7
Nam Nguyễn <nguyenphinam2k2@gmail.com>
2023-03-19 10:14:00 +0700

    first commit

---
+++ setting.py
@@ -0,0 +1,18 @@
+level_map = [
+'       X  XXXX    XX  XXX   ',
+' XX     XXXXX               ',
+'                            ',
+' XX    XXX            XX    ',
+' XX P                       ',
+' XXXX         XX         XX ',
+' XXXX       XX              ',
+' XX    X  XXXX    XX  XX    ',
+'       X  XXXX    XX  XXX   ',
+'    XXXX  XXXXXX  XX  XXXX  ',
+'XXXXXXXX  XXXXXX  XX  XXXX  ']
+
+vertical_tile_number = 11
+tile_size = 64 
+
+screen_height = vertical_tile_number * tile_size
+screen_width = 1200
